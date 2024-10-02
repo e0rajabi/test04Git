e93ba0ca0de78969d0b82bc05d6769f72e206974
elham <e0rajabi@gmail.com>
2024-10-02 10:53:28 +0330

    up03

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>khodaya be omide to</p>
         <p>elham rajabi</p>
         <p>roham and ryan jooooooni </p>
       </header>
